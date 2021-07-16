26121a0c15196b2ed098beaebfbf6cdef37caa0f
kmou424 <me@kmou424.moe>
2021-07-16 17:57:50 +0800

    [bin]compress_enhanced: Add auto install executable files

---
+++ bin/install.sh
@@ -0,0 +1,4 @@
+cp compress_image_enhanced /bin/compress_image_enhanced
+cp compress_video_enhanced /bin/compress_video_enhanced
+chmod +x /bin/compress_image_enhanced
+chmod +x /bin/compress_video_enhanced
